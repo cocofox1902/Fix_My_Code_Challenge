b3eb17d04beab6e769397611137981918d5ca327
cocofox1902 <colas.renard1902@gmail.com>
2022-05-30 15:06:04 +0000

    add task square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 class square():
+    """square"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """square.__init__()"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,13 +15,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """square.__str__()"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
+    """main"""
 
     s = square(width=12, height=9)
     print(s)
